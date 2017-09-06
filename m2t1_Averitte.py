e8e684a3544bbc9356f39ccf280d8b76c4787c7c
averittk4249 <31700873+averittk4249@users.noreply.github.com>
2017-09-06 11:17:44 -0400

    Add files via upload
    First submission

---
+++ m2t1_Averitte.py
@@ -0,0 +1,44 @@
+Python 3.5.2 (v3.5.2:4def2a2901a5, Jun 25 2016, 22:01:18) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print ('hello')
+hello
+>>> print (10*8)
+80
+>>> print (8+4+2+1)
+15
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> 
+>>> myNumber = 5
+>>> myNumber
+5
+>>> myName = "Ken"
+>>> print (myName)
+Ken
+>>> 
+======= RESTART: C:/Program Files (x86)/Python35-32/PythonTutorial.py =======
+The answer is 18
+>>> 
+======= RESTART: C:/Program Files (x86)/Python35-32/PythonTutorial.py =======
+Type a numer: 
+======= RESTART: C:/Program Files (x86)/Python35-32/PythonTutorial.py =======
+Type a numer: 
+======= RESTART: C:/Program Files (x86)/Python35-32/PythonTutorial.py =======
+Type a number: 
+======= RESTART: C:/Program Files (x86)/Python35-32/PythonTutorial.py =======
+Type a number: 
+======= RESTART: C:/Program Files (x86)/Python35-32/PythonTutorial.py =======
+23.0
+Type a number: 
